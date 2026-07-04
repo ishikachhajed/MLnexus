9a90de6c11f49e4251e93246a8faf5a0a8453148
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-07-04 08:13:16 +0530

    Vite React setup for Frontend

---
+++ app/frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { BrowserRouter, Routes, Route, Link, useNavigate } from "react-router-dom";
+import { getUser, clearAuth } from "./utils/api";
+import type { User } from "./utils/api";
+
+export default function App() {
+  const [user, setUser] = useState<User | null>(getUser());
+
+  const handleLogin = () => setUser(getUser());
+  const handleLogout = () => {
+    clearAuth(); setUser(null);
+  };
+
+  return (
+    <BrowserRouter>
+      <Routes>
+      </Routes>
+    </BrowserRouter>
+  )
+}
