ce6c91ae55e05b472d7940c262dc48f23a78ee2b
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-07-06 05:11:52 +0530

    Create page for registration and connected to backend

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { getUser, clearAuth } from "./utils/api";
 import type { User } from "./utils/api";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
+import Register from "./pages/Register";
 
 export default function App() {
   const [user, setUser] = useState<User | null>(getUser());
@@ -18,6 +19,7 @@ export default function App() {
       <Navbar user={user} onLogout={handleLogout} />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/register" element={<Register onLogin={handleLogin} />} />
       </Routes>
     </BrowserRouter>
   )
