92797c86d36a76764766d03d69710df55afbc343
Ishika Chhajed <ishikachhajed68@gmail.com>
2026-07-06 09:34:28 +0530

    Added logi functionality

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -5,6 +5,7 @@ import type { User } from "./utils/api";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 export default function App() {
   const [user, setUser] = useState<User | null>(getUser());
@@ -20,6 +21,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/register" element={<Register onLogin={handleLogin} />} />
+        <Route path="/login" element={<Login onLogin={handleLogin} />} />
       </Routes>
     </BrowserRouter>
   )
